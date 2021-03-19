9f56af27b39c730de3b90e38f0822f9e8cc76e7a
yarbest <72338354+yarbest@users.noreply.github.com>
2021-03-19 17:52:45 +0200

    1

--- src/js/main.js
+++ src/js/main.js
@@ -14,10 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const modalWindows = () => {
         const modalPreorder = document.querySelector(".modal-preorder");
-
+        const btnOpenModalPreorder = document.querySelector(".film-description__pre-order");
         const modalTrailer = document.querySelector(".modal-trailer");
         const btnOpenModalTrailer = document.querySelector(".film-description__trailer");
-
         const videoFrame = modalTrailer.querySelector("iframe");
 
         btnOpenModalTrailer.addEventListener("click", () => {
@@ -30,6 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
             if (target.matches(".modal-trailer") || target.matches(".modal-trailer__btn-close")) {
                 modalTrailer.classList.remove("active");
                 videoFrame.src = "";
+            }
+        });
+
+        btnOpenModalPreorder.addEventListener("click", () => {
+            modalPreorder.classList.add("active");
+        });
+
+        modalPreorder.addEventListener("click", (event) => {
+            const target = event.target;
+            if (target.matches(".modal-preorder") || target.matches(".modal-preorder__btn-close")) {
+                modalPreorder.classList.remove("active");
             }
         });
     };
